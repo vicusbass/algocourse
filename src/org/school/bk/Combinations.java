3bc142f3709eb33d25b027e6a09eb867bb4d2676
vicus <vasile.pop@gmail.com>
2017-06-28 07:33:38 +0300

    some c

--- src/org/school/bk/Combinations.java
+++ src/org/school/bk/Combinations.java
@@ -6,32 +6,24 @@ public class Combinations {
     private static int[] stiva = new int[50];
 
     public static void main(String[] args) {
-        permute(1);
+        combine(1);
     }
 
     private static void write() {
-        for (int i = 1; i < n + 1; i++) {
+        for (int i = 1; i < k + 1; i++) {
             System.out.print(stiva[i] + " ");
         }
         System.out.println();
     }
 
-    private static boolean valid(int k) {
-        for (int i = 1; i < k; i++) {
-            if (stiva[i] == stiva[k]) return false;
-        }
-        return true;
-    }
 
-    private static void permute(int i) {
-        for (int j = 1; j <= n; j++) {
-            stiva[i] = j;
-            if (valid(i)) {
-                if (i == n) {
-                    write();
-                } else {
-                    permute(i + 1);
-                }
+    private static void combine(int x) {
+        for (int i = stiva[x - 1] + 1; i <= n - k + x; i++) {
+            stiva[x] = i;
+            if (x == k) {
+                write();
+            } else {
+                combine(x + 1);
             }
         }
 
